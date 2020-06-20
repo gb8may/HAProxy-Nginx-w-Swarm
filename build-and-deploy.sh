671edbf67eb909228e4a894b26b34de7843c1458
Mayara Gouveia <35708820+gb8may@users.noreply.github.com>
2020-06-20 18:34:07 -0300

    Update build-and-deploy.sh

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 docker build -t haproxy-challenge:1.8 .
+#Add tag to rename Docker image
 docker tag haproxy:1.8 haproxy-challenge:1.8
 docker stack deploy -c docker-stack.yaml challenge
